721f7e89642d123044247ad629aefb6cf857b3c4
digitalkfmw <digitalk.fmw@gmail.com>
2025-03-06 10:57:39 +0530

    Added unit tests and updated pom.xml for CI

---
+++ src/test/java/com/AppTest.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true); // Simple test always passing
+    }
+}
